5e23fbcc4d32e683cce7efed450cc7623bf40840
rbible <rbible@qq.com>
2015-10-10 18:33:26 +0800

    format
    format

--- src/main/java/com/winterbe/java8/samples/time/LocalTime1.java
+++ src/main/java/com/winterbe/java8/samples/time/LocalTime1.java
@@ -15,58 +15,50 @@ import java.util.Locale;
  */
 public class LocalTime1 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        // get the current time
-        Clock clock = Clock.systemDefaultZone();
-        long t0 = clock.millis();
-        System.out.println(t0);
+		// get the current time
+		Clock clock = Clock.systemDefaultZone();
+		long t0 = clock.millis();
+		System.out.println(t0);
 
-        Instant instant = clock.instant();
-        Date legacyDate = Date.from(instant);
+		Instant instant = clock.instant();
+		Date legacyDate = Date.from(instant);
+		System.out.println(legacyDate);
 
+		ZoneId zone1 = ZoneId.of("Europe/Berlin");
+		ZoneId zone2 = ZoneId.of("Brazil/East");
 
-        ZoneId zone1 = ZoneId.of("Europe/Berlin");
-        ZoneId zone2 = ZoneId.of("Brazil/East");
+		System.out.println(zone1.getRules());
+		System.out.println(zone2.getRules());
 
-        System.out.println(zone1.getRules());
-        System.out.println(zone2.getRules());
+		// time
+		LocalTime now1 = LocalTime.now(zone1);
+		LocalTime now2 = LocalTime.now(zone2);
 
-        // time
-        LocalTime now1 = LocalTime.now(zone1);
-        LocalTime now2 = LocalTime.now(zone2);
+		System.out.println(now1);
+		System.out.println(now2);
 
-        System.out.println(now1);
-        System.out.println(now2);
+		System.out.println(now1.isBefore(now2)); // false
 
-        System.out.println(now1.isBefore(now2));  // false
+		long hoursBetween = ChronoUnit.HOURS.between(now1, now2);
+		long minutesBetween = ChronoUnit.MINUTES.between(now1, now2);
+		System.out.println(hoursBetween);
+		System.out.println(minutesBetween);
 
-        long hoursBetween = ChronoUnit.HOURS.between(now1, now2);
-        long minutesBetween = ChronoUnit.MINUTES.between(now1, now2);
-        System.out.println(hoursBetween);
-        System.out.println(minutesBetween);
+		// create time
 
+		LocalTime now = LocalTime.now();
+		System.out.println(now);
 
-        // create time
+		LocalTime late = LocalTime.of(23, 59, 59);
+		System.out.println(late);
 
-        LocalTime now = LocalTime.now();
-        System.out.println(now);
+		DateTimeFormatter germanFormatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).withLocale(Locale.GERMAN);
 
-        LocalTime late = LocalTime.of(23, 59, 59);
-        System.out.println(late);
+		LocalTime leetTime = LocalTime.parse("13:37", germanFormatter);
+		System.out.println(leetTime);
 
-        DateTimeFormatter germanFormatter =
-                DateTimeFormatter
-                        .ofLocalizedTime(FormatStyle.SHORT)
-                        .withLocale(Locale.GERMAN);
-
-        LocalTime leetTime = LocalTime.parse("13:37", germanFormatter);
-        System.out.println(leetTime);
-
-
-        // to legacy date
-
-
-    }
-
+		// to legacy date
+	}
 }
